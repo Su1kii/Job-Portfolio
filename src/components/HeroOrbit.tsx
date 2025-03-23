4119d4cf285cb53d6cb23f7a85bd377793680b29
Steven Echeverria <stevennava749@gmail.com>
2025-03-23 13:19:04 -0600

    Update HeroOrbit.tsx

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -15,14 +15,14 @@ export const HeroOrbit = ({
   shouldOrbit?: boolean;
   shouldSpin?: boolean;
   spinDuration?: string;
-  spinduration?: string;
+  orbitDuration?: string; // Added this line to ensure orbitDuration is part of the props
 }>) => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
       <div
         className={twMerge(shouldOrbit === true && "animate-spin")}
         style={{
-          animationDuration: orbitDuration,
+          animationDuration: orbitDuration, // Use the orbitDuration prop
         }}
       >
         <div
@@ -33,9 +33,12 @@ export const HeroOrbit = ({
             width: `${size}px`,
           }}
         >
-          <div className={twMerge(shouldSpin === true && "animate-spin")} style={{
-            animationDuration: spinDuration
-          }}>
+          <div
+            className={twMerge(shouldSpin === true && "animate-spin")}
+            style={{
+              animationDuration: spinDuration, // Use the spinDuration prop
+            }}
+          >
             <div
               className="inline-flex"
               style={{
